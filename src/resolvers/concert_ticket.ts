a3ba5815e8ce3e0280c69767d372b7efffbc43c5
Daniel Tran <129140506+DanielvTran@users.noreply.github.com>
2024-01-21 12:39:50 +1000

    Redeem QR Code
    Change the value of attend to true when the redeem function is run.

---
+++ src/resolvers/concert_ticket.ts
@@ -0,0 +1,28 @@
+import { Types } from "mongoose";
+import _ from "lodash";
+import { ConcertTicket } from "../models/concert_ticket";
+import type { TypedResolvers } from "../types/GraphQL";
+import { concertTicketApi } from "../services/concertTicket";
+
+const { ObjectId } = Types;
+
+const concertTicketResolver: TypedResolvers = {
+  Query: {
+    concertTickets: async () => {
+      return ConcertTicket.find({});
+    },
+    concertTicketsByID: async (root, { _id }) => {
+      return ConcertTicket.findOne({ _id: new ObjectId(_id) });
+    },
+  },
+  Mutation: {
+    redeemQRCode: (root, { concert_ID }) => {
+      return concertTicketApi.redeemQRCode(concert_ID);
+    },
+    // login: (root, { email, password }) => {
+    //   return usersApi.login(email, password);
+    // },
+  },
+};
+
+export default concertTicketResolver;
